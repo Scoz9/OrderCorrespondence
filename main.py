74e8191fa1eae6cf5a1b06ba62af1302de73626e
Vincenzo Scozzari <vincenzo.scoz9@gmail.com>
2024-03-09 18:49:38 +0100

    feat(main.py): Implement ordersMultiPack logic
    transfer extract_product_quantity and extract_product_shipping_address functions to extract_product_details.Implement group_same_products function that groups singleProducts with the same name by adding the quantities and multiProducts with the same name and same quantity by counting the number of units.

--- main.py
+++ main.py
@@ -102,27 +102,37 @@ def extract_text_from_pdf(pdf_path):
         for page_num in range(len(pdf_file)):
             page = pdf_file.load_page(page_num)
             page_text = page.get_text()
-            shipping_address = extract_shipping_address(page_text)
-            product_details = extract_product_details(page_text)
-            quantity = extract_product_quantity(page_text)
-            if shipping_address and product_details:
-                all_elements.append((shipping_address, product_details, quantity))
             
+            product_details = extract_product_details(page_text)           
+            all_elements.append(product_details)
     return all_elements
 
-def extract_shipping_address(text):
-    match = re.search(r"Spedire a:\n(.*?)(?=\n)", text)
-    if match:
-        return match.group(1).strip()[:22].upper()
-    return None
-
-
 def extract_product_details(text):
-    match = re.search(r"Totale ordine\n\d+\n(.*?)(?=\nSKU:)", text, re.DOTALL)
-    if match:
-        return orderDistinction(re.sub(r"[\s-]+", " ", match.group(1))[5:])
-    return None
+    product = []
 
+    match_shipping_address = re.search(r"Spedire a:\n(.*?)(?=\n)", text)
+    shipping_address = match_shipping_address.group(1).strip()[:22].upper()
+
+    match_nome_prod1 = re.search(r"Totale ordine\n\d+\n(.*?)(?=\nSKU:)", text, re.DOTALL)
+    nome_prod1 = orderDistinction(re.sub(r"[\s-]+", " ", match_nome_prod1.group(1))[5:]) 
+
+    match_quantity_prod1 = re.search(r"Totale ordine\n(\d+)\n", text)
+    quantity_prod1 = match_quantity_prod1.group(1).strip()
+
+    match_quantity_prod2 = re.search(r'Tot\. articolo.*\n(?P<quantity>\d+)\n.*\nSKU', text, re.DOTALL)
+    if match_quantity_prod2:
+        quantity_prod2 = match_quantity_prod2.group(1).strip()
+        match_nome_prod2 = re.search(r'Tot\. articolo\n(?:.*\n)*[0-9]+\n(.*?)(?=SKU:)', text, re.DOTALL)
+        nome_prod2 = orderDistinction(re.sub(r"[\s-]+", " ", match_nome_prod2.group(1))[5:])
+
+    if not match_quantity_prod2:
+        product.append((shipping_address, nome_prod1, quantity_prod1))
+        return product
+
+    product.append((shipping_address, nome_prod1, quantity_prod1))
+    product.append((shipping_address, nome_prod2, quantity_prod2))
+
+    return product
 
 def orderDistinction(product_name):
     max_length = 40
@@ -142,49 +152,90 @@ def orderDistinction(product_name):
         # Taglia la parte di stringa prima delle parentesi tonde se necessario
         if length_before_parentheses > max_length_before_parentheses:
             product_name = product_name[:max_length_before_parentheses]
+
         return product_name[:max_length_before_parentheses] + content_in_parentheses
 
     # Se non ci sono parentesi tonde nella stringa, restituisci la stringa stessa
     return product_name[:max_length]
 
 
-def extract_product_quantity(text):
-    match = re.search(r"Totale ordine\n(\d+)\n", text)
-    if match:
-        return match.group(1).strip()
-    return None
-
 def add_product_name_to_pdf(pdf_path, listOrders):
+    singleProducts = []
+    multiProducts = []
+    ordersMultiProduct = []
+
     with fitz.open(pdf_path) as pdf_document:
         # Pre-calculate all page texts
         page_texts = [re.sub(r'\s+', ' ', page.get_text()) for page in pdf_document]
         
         for order in listOrders:
-            shipping_address_find = any(order[0] in page_text for page_text in page_texts)
+            shipping_address_find = any(order[0][0] in page_text for page_text in page_texts)
             if not shipping_address_find:
                 continue
             
             # Get the first page containing the shipping address
-            page_index = next(i for i, page_text in enumerate(page_texts) if order[0] in page_text)
+            page_index = next(i for i, page_text in enumerate(page_texts) if order[0][0] in page_text)
             page = pdf_document[page_index]
             page.wrap_contents()
             page.set_rotation(180)
+
             # Get the rectangle coordinates of the shipping address
-            rect = page.search_for(order[0])[0]
-           
+            rect = page.search_for(order[0][0])[0]
+
             # Define offset for x and y coordinates in each quadrants
             x_offset = 267 if 0 < rect[0] < 300 else 565
             y_offset = 90 if 0 < rect[1] < 415 else 510
 
-            if order[2] != "1": 
-                page.insert_text((x_offset, y_offset), text=order[1], fontsize=10, rotate=180, render_mode=0)
-                page.insert_text((x_offset - 185, y_offset), text=" --> x" + str(order[2]), fontsize=15, rotate=180, render_mode=0)
+            if len(order) > 1:
+                ordersMultiProduct.append(order)
+                for item in order:
+                    page.insert_text((x_offset, y_offset), text=item[1], fontsize=10, rotate=180, render_mode=0)
+                    if item[2] != "1":
+                        page.insert_text((x_offset - 185, y_offset), text=" --> x" + str(item[2]), fontsize=15, rotate=180, render_mode=0) 
+                    y_offset+= 10
+            elif order[0][2] != "1": 
+                page.insert_text((x_offset, y_offset), text=order[0][1], fontsize=10, rotate=180, render_mode=0)
+                page.insert_text((x_offset - 185, y_offset), text=" --> x" + str(order[0][2]), fontsize=15, rotate=180, render_mode=0)
+                multiProducts.append(order)
             else:
-                page.insert_text((x_offset, y_offset), text=order[1], fontsize=10, rotate=180, render_mode=0)
+                page.insert_text((x_offset, y_offset), text=order[0][1], fontsize=10, rotate=180, render_mode=0)
+                singleProducts.append(order)
+
+        singleProducts, multiProducts = group_same_products(singleProducts, multiProducts)
 
         # Save the modified PDF
         pdf_document.saveIncr()
 
+def group_same_products(singleProducts, multiProducts):
+    singleProdDict = {}
+    multipleProdDict = {}
+
+    # Raggruppa i prodotti per nome e somma le quantità
+    for item in singleProducts:
+        productName = item[0][1]
+        quantity = int(item[0][2])
+        if productName in singleProdDict:
+            singleProdDict[productName] += quantity
+        else:
+            singleProdDict[productName] = quantity
+
+    # Raggruppa i prodotti multipli per nome e quantità
+    for item in multiProducts:
+        product_name = item[0][1]
+        quantity = int(item[0][2])
+        key = (product_name, quantity)
+        if key in multipleProdDict:
+            multipleProdDict[key] += 1
+        else:
+            multipleProdDict[key] = 1
+
+    # Costruisci la lista di prodotti sommati
+    singleProducts = [(productName, str(quantity)) for productName, quantity in singleProdDict.items()]
+
+    # Costruisci la lista di prodotti multipli raggruppati
+    multiProducts = [('', product_name, f"(x{quantity})") if count == 1 else (f"(x{count})", product_name, f"(x{quantity})") for (product_name, quantity), count in multipleProdDict.items()]
+
+    return singleProducts, multiProducts
 
 def main():
     guiObj = Gui()
